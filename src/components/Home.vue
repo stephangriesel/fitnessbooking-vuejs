8c8ab091a9780f37e3c92f84622cd48d37b9ecdd
Stephan Griesel <sgriesel@gmail.com>
2022-10-25 06:51:43 +0200

    layout set up

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,46 @@
   <div class="hello">
     <!-- <h1>{{ msg }}</h1> -->
     <div class="block">
-      <div>left</div>
-      <div>right</div>
+      <header>
+        <nav>
+          <div>Logo</div>
+          <div>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <section>
+        <div>left</div>
+        <div>right</div>
+      </section>
+
+      <footer>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Reschedule</li>
+            <li>Cancellation</li>
+            <li>Refund</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Amsterdam</li>
+            <li>Berlin</li>
+            <li>London</li>
+          </ul>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
@@ -21,15 +59,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
+
 a {
   color: #42b983;
 }
-.block {
+
+nav{
   display:flex;
+  justify-content: space-between;
+}
+
+section{
+  display:flex;
+  justify-content: space-between;
+}
+
+footer{
+  display: flex;
   justify-content: space-evenly;
-  align-items: center;
 }
 </style>
